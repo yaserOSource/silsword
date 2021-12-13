6fe5e3d77db5169b6e1aeace9a15c04638b9a3ce
Avaer Kazmer <a@modules.io>
2021-12-13 02:21:08 -0500

    Fix physics

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 import * as THREE from 'three';
-import Simplex from './simplex-noise.js';
+// import Simplex from './simplex-noise.js';
 import metaversefile from 'metaversefile';
-const {useFrame, useApp} = metaversefile;
+const {useApp, useScene} = metaversefile;
 
 const baseUrl = import.meta.url.replace(/(\/)[^\/\\]*$/, '$1');
 
-const localVector = new THREE.Vector3();
-const simplex = new Simplex('lol');
-
 export default () => {
   const app = useApp();
+  const scene = useScene();
 
   let subApp = null;
   (async () => {
-    subApp = await metaversefile.load(baseUrl + '/megasword_v4_texta.glb');
-    app.add(subApp);
+    const u2 = baseUrl + '/megasword_v4_texta.glb';
+    const m = await metaversefile.import(u2);
+
+    subApp = metaversefile.createApp({
+      name: u2,
+    });
+    subApp.position.copy(app.position);
+    subApp.quaternion.copy(app.quaternion);
+    subApp.scale.copy(app.scale);
+    subApp.updateMatrixWorld();
+    // subApp.instanceId = getNextInstanceId();
+    subApp.contentId = u2;
+    subApp.setComponent('physics', true);
+    /* for (const {key, value} of components) {
+      subApp.setComponent(key, value);
+    } */
+    await subApp.addModule(m);
+    scene.add(subApp);
+
+    // metaversefile.addApp(subApp);
+    // window.subApp = subApp;
+
+    // app.add(subApp);
   })();
 
-  app.getPhysicsObjects = () => subApp ? subApp.getPhysicsObjects() : [];
+  app.getPhysicsObjects = () => {
+    const result = subApp ? subApp.getPhysicsObjects() : [];
+    // console.log('get physics objects', subApp, result);
+    return result;
+  };
 
   return app;
 };
