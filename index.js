1cc7513824f4edf62286147c0ea40dc2c930f28d
Avaer Kazmer <a@modules.io>
2021-12-13 22:54:39 -0500

    Code cleanup

--- index.js
+++ index.js
@@ -8,45 +8,7 @@ const baseUrl = import.meta.url.replace(/(\/)[^\/\\]*$/, '$1');
 export default () => {
   const app = useApp();
   const scene = useScene();
-
-  /* const components = [
-    {
-      "key": "physics",
-      "value": true
-    },
-    {
-      "key": "animation",
-      "value": false
-    },
-    {
-      "key": "wear",
-      "value": {
-        "boneAttachment": "spine",
-        "position": [-0.3, 0.7, -0.15],
-        "quaternion": [0, 0, 0.9510565162951536, -0.30901699437494734]
-      }
-    },
-    {
-      "key": "use",
-      "value": {
-        "animation": [
-          "swordSideSlash",
-          "swordSideSlash"
-        ],
-        "boneAttachment": "leftHand",
-        "position": [-0.07, -0.03, 0],
-        "quaternion": [0.7071067811865475, 0, 0, 0.7071067811865476],
-        "scale": [1, 1, 1]
-      }
-    },
-    {
-      "key": "aim",
-      "value": {
-        "animation": "swordSide",
-      },
-    }
-  ]; */
-  // console.log('got app components', app.components);
+  
   const {components} = app;
 
   let subApp = null;
@@ -63,21 +25,12 @@ export default () => {
     subApp.updateMatrixWorld();
     subApp.contentId = u2;
     subApp.instanceId = app.instanceId;
-    // subApp.instanceId = getNextInstanceId();
 
-    // subApp.setComponent('physics', true);
     for (const {key, value} of components) {
       subApp.setComponent(key, value);
     }
     await subApp.addModule(m);
     scene.add(subApp);
-
-    // metaversefile.addApp(subApp);
-    // window.subApp = subApp;
-
-    // app.add(subApp);
-
-    // window.subApp = subApp;
   })();
 
   useActivate(() => {
@@ -87,23 +40,16 @@ export default () => {
 
   useWear(e => {
     const {wear} = e;
-    // for (const subApp of subApps) {
-    console.log('wear', subApp, wear);
     if (subApp) {
       subApp.dispatchEvent({
         type: 'wearupdate',
         wear,
       });
     }
-    // }
-    // wearing = wear;
   });
-
-  // window.app = app;
 
   app.getPhysicsObjects = () => {
     const result = subApp ? subApp.getPhysicsObjects() : [];
-    // console.log('get physics objects', subApp, result);
     return result;
   };
 
